efa59f47ad3528f440c66eee1d63f64da125c3ee
minjae <minjae4650@gmail.com>
2024-12-26 21:18:18 +0900

    feat: sample data 수정

--- app/src/main/java/com/example/bookapp/MainActivity.kt
+++ app/src/main/java/com/example/bookapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.bookapp
 
 import com.example.bookapp.tab2.BookAdapter
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,18 +11,16 @@ import com.example.bookapp.tab2.Book
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         // 샘플 데이터
         val books = listOf(
-            Book("The Great Gatsby", "F. Scott Fitzgerald"),
-            Book("To Kill a Mockingbird", "Harper Lee"),
-            Book("1984", "George Orwell"),
-            Book("Pride and Prejudice", "Jane Austen"),
-            Book("The Catcher in the Rye", "J.D. Salinger")
+            Book("서울에 밤이 내렸을 때", "지은이1", R.drawable.book1),
+            Book("서울에 삽니다", "지은이2", R.drawable.book2),
+            Book("다시 서울", "지은이3", R.drawable.book3),
+            Book("한강길 따라 서울 둘러보기", "지은이4", R.drawable.book4),
+            Book("내가 알지 못했던 도심 속 서울", "지은이5", R.drawable.book5)
         )
-
         // RecyclerView 초기화
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this) // 세로 방향 기본 설정
