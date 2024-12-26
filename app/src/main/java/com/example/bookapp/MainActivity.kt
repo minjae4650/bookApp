44c6c78f2c49c211c8a76fce4a5caba7b54b6978
minjae <minjae4650@gmail.com>
2024-12-26 20:10:24 +0900

    feat: sample data 넣어서 확인

--- app/src/main/java/com/example/bookapp/MainActivity.kt
+++ app/src/main/java/com/example/bookapp/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.bookapp
 
+import com.example.bookapp.tab2.BookAdapter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookapp.tab2.Book
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // 샘플 데이터
+        val books = listOf(
+            Book("The Great Gatsby", "F. Scott Fitzgerald"),
+            Book("To Kill a Mockingbird", "Harper Lee"),
+            Book("1984", "George Orwell"),
+            Book("Pride and Prejudice", "Jane Austen"),
+            Book("The Catcher in the Rye", "J.D. Salinger")
+        )
+
+        // RecyclerView 초기화
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this) // 세로 방향 기본 설정
+        recyclerView.adapter = BookAdapter(books) // 어댑터 연결
     }
 }
