869bd3fab10c5d47ae912848341dd6507c91b911
minjae <minjae4650@gmail.com>
2024-12-26 20:09:45 +0900

    feat: BookAdapter 클래스 생성
    - 간단히 TextView에 제목과 저자만 표현

---
+++ app/src/main/java/com/example/bookapp/tab2/BookAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.bookapp.tab2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookapp.R
+
+class BookAdapter(
+    private val books: List<Book>
+) : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+
+    class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleTextView: TextView = itemView.findViewById(R.id.bookTitle)
+        val authorTextView: TextView = itemView.findViewById(R.id.bookAuthor)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+        return BookViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        val book = books[position]
+        holder.titleTextView.text = book.title
+        holder.authorTextView.text = book.author
+    }
+
+    override fun getItemCount(): Int = books.size
+}
