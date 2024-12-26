acf0b341489546cd153d239ce92d6fb49c4cc2eb
minjae <minjae4650@gmail.com>
2024-12-26 21:18:09 +0900

    feat: Adapter에 ImageView 추가

--- app/src/main/java/com/example/bookapp/tab2/BookAdapter.kt
+++ app/src/main/java/com/example/bookapp/tab2/BookAdapter.kt
@@ -3,6 +3,7 @@ package com.example.bookapp.tab2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bookapp.R
@@ -12,6 +13,7 @@ class BookAdapter(
 ) : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
 
     class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.bookImage)
         val titleTextView: TextView = itemView.findViewById(R.id.bookTitle)
         val authorTextView: TextView = itemView.findViewById(R.id.bookAuthor)
     }
@@ -25,6 +27,7 @@ class BookAdapter(
         val book = books[position]
         holder.titleTextView.text = book.title
         holder.authorTextView.text = book.author
+        holder.imageView.setImageResource(book.imagePath)
     }
 
     override fun getItemCount(): Int = books.size
