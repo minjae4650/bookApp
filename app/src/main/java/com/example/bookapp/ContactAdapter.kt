808246c3c8b1fdc2faa5bccb4198a7b2181cd7de
doleebest <sj27419@gmail.com>
2024-12-26 17:58:39 +0900

    contactadapter

---
+++ app/src/main/java/com/example/bookapp/ContactAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.bookapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter(private val contactList: MutableList<Contact>) :
+    RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+
+    class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.contactName)
+        val phoneTextView: TextView = itemView.findViewById(R.id.contactPhone)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent, false)
+        return ContactViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = contactList[position]
+        holder.nameTextView.text = contact.name
+        holder.phoneTextView.text = contact.phone
+    }
+
+    override fun getItemCount() = contactList.size
+}
