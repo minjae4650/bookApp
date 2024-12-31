1e069b695d87c88dca5427e868f77ede581cde01
minjae <minjae4650@gmail.com>
2025-01-01 01:44:46 +0900

    feat: Comment Adapter 추가

---
+++ app/src/main/java/com/example/bookapp/tab2/CommentAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.bookapp.tab2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookapp.R
+
+class CommentAdapter(
+    private val comments: MutableList<String>
+) : RecyclerView.Adapter<CommentAdapter.CommentViewHolder>() {
+
+    inner class CommentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val commentText: TextView = itemView.findViewById(R.id.commentTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_comment, parent, false)
+        return CommentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
+        val comment = comments[position]
+        holder.commentText.text = comment
+    }
+
+    override fun getItemCount(): Int = comments.size
+}
